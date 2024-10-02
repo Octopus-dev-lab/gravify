300f9f2452dcdc366a776e5834ec7d3a705e2f63
Jurgen <jurgen.alla.cs@gmail.com>
2024-10-02 18:52:08 +0200

    Feat: add page_extraction
    Add create_pages() :
    - scan project/public for .html files
    - create a dict with (int) pages' indexes as keys and pages' info as values
    - create a dir and a .md file for each html file (in user/pages)
    - copy all html files as .twig files in project-theme/templates

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 from grav_setup import setup_grav
+from page_extraction import create_pages
 
 if __name__ == '__main__':
     os.chdir('data')
     setup_grav()
+    pages = create_pages()
+
---
+++ src/page_extraction.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+
+def get_grav_md_page_content(page_name):
+    return f'---\ntitle: {page_name}\n---\n'
+
+def to_grav_page_name(page_name):
+    grav_page_name = page_name.split('-')[1]
+    grav_page_name = grav_page_name.replace('.html', '.md')
+    return grav_page_name
+
+def to_grav_dir_name(name):
+    name = name.replace('.html', '')
+    name = name.replace('-', '.')
+    return name
+
+def to_grav_template_name(page_name):
+    template_name = page_name.split('-')[1]
+    return f'{template_name}.twig'
+
+def find_pages():
+    all_files = os.listdir('project/public')
+    pages = []
+
+    for file in all_files:
+        if file.endswith('.html'):
+            pages.append(file)
+
+    return pages
+
+def create_grav_md_pages(pages):
+    for _, page in pages.items():
+        dir_name = page['dir']
+        page_name = page['page']
+        grav_page_content = get_grav_md_page_content(page_name)
+        
+        os.mkdir(f'project-output/user/pages/{dir_name}')
+        with open(f'project-output/user/pages/{dir_name}/{page_name}', 'w') as file:
+            file.write(grav_page_content)
+            file.close()
+        
+
+def create_grav_template_pages(pages):
+    for _, page in pages.items():
+        template_name = page['template']
+        page_name = page['src_name']
+        
+        shutil.copy(f'project/public/{page_name}', f'project-output/user/themes/project-theme/templates/{template_name}.html.twig')
+    
+def create_grav_pages(pages):
+    create_grav_md_pages(pages)
+    create_grav_template_pages(pages)
+
+def build_pages_dict(pages_names):
+    pages = {}
+    for page_name in pages_names:
+        grav_page_index = int(page_name.split('-')[0])
+        grav_page_dir = to_grav_dir_name(page_name)
+        grav_page_name = to_grav_page_name(page_name)
+        grav_template_name = to_grav_template_name(page_name)
+
+        if grav_page_index in pages:
+            shutil.rmtree(f'project-output')
+            raise Exception(f'Duplicate index {grav_page_index} in html files')
+        else:
+            pages[grav_page_index] = {
+                'src_name' : page_name,
+                'dir': grav_page_dir,
+                'page': grav_page_name,
+                'template': grav_template_name
+            }
+    return pages
+
+def create_pages():
+    pages_names = find_pages()
+    pages = build_pages_dict(pages_names)
+    create_grav_pages(pages)
+    return pages
