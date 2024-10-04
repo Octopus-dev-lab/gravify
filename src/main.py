95d54dfbe4fb5cfe40b998f7f3ddeeb8fc68cd5a
Jurgen <jurgen.alla.cs@gmail.com>
2024-10-04 19:19:31 +0200

    feat: add build_blueprints to main

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ from grav_setup import setup_grav
 from page_extraction import create_pages
 from template_page import template_pages
 from res_distribution import distribute_files
+from blueprint_build import build_blueprints
 
 if __name__ == '__main__':
     os.chdir('data')
     setup_grav()
     pages = create_pages()
     pages_templates = template_pages(pages)
-    distribute_files(pages_templates)
+    build_blueprints(pages_templates)
+    distribute_files(pages, pages_templates)
