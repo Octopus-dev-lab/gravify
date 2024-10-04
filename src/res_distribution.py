38c6a0e1c0d07addc50bf43857c8d4fe68eb82a2
Jurgen <jurgen.alla.cs@gmail.com>
2024-10-04 19:20:10 +0200

    Feat: add distribution of twig fields' assets

--- src/res_distribution.py
+++ src/res_distribution.py
@@ -13,13 +13,30 @@ def distribute_theme_assets(theme_assets):
     os.mkdir(f'project-output/user/themes/project-theme/assets')
     distribute_by_file_type(theme_assets['assets'], 'assets')
 
-def distribute_files(pages_templates):
+def distribute_twig_assets(page_dir, twig_assets):
+    for _, asset in twig_assets.items():
+        attrib_name = ''
+        if asset['type'] == 'img' or asset['type'] == 'video' or asset['type'] == 'audio':
+            attrib_name = 'src'
+        if asset['type'] == 'head_icon':
+            attrib_name = 'href'
+
+        if attrib_name != '' and 'http' not in asset[attrib_name]:
+                asset_name = asset[attrib_name].split("/")[-1]
+                print(f'   * project/public/{asset[attrib_name]} -> project-output/user/pages/{page_dir}/{asset_name}')
+                shutil.copy2(f'project/public/{asset[attrib_name]}', f'project-output/user/pages/{page_dir}/{asset_name}')
+        
+
+def distribute_files(pages, pages_templates):
     print('Distributing files')
-    for template, all_assets in pages_templates.items():
-        print(f'  - {template}')
-        theme_assets = all_assets['theme']
-        twig_assets = all_assets['editable']
+    for index, pages in pages.items():
+        page_name = pages['template'].split('.')[0]
+        page_dir = pages['dir']
+        print(f'  - {page_name}')
+        theme_assets = pages_templates[page_name]['theme']
+        twig_assets = pages_templates[page_name]['editable']
         distribute_theme_assets(theme_assets)
+        distribute_twig_assets(page_dir, twig_assets)
         
         
 
