84bcf881ddaf9e90cb4f8131906669f30a580cbf
Jurgen <jurgen.alla.cs@gmail.com>
2024-10-31 16:16:45 +0100

    Feat: read config file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 import os
+import json
 from grav_setup import setup_grav
 from page_extraction import create_pages
 from template_page import template_pages
 from res_distribution import distribute_files
 from blueprint_build import build_blueprints
 
+def read_config():
+    print('Reading config file...', end=' ')
+    config = {}
+    
+    try:
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+        print('OK')
+    except FileNotFoundError:
+        with open('../default-config.json', 'r') as file:
+            config = json.load(file)
+        print('OK (default-config.json)')
+    
+    return config
+
 if __name__ == '__main__':
     os.chdir('data')
+    config = read_config()
     setup_grav()
     pages = create_pages()
     pages_templates = template_pages(pages)
